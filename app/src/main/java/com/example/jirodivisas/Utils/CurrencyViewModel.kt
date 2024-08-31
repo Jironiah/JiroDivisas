2b8641f5c47a82e2d62a94298a99e3bf0e0a883d
Jironiah <seandertal@gmail.com>
2024-08-31 20:16:26 +0200

    Utils to load the dropdowns and do the conversion

---
+++ app/src/main/java/com/example/jirodivisas/Utils/CurrencyViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.jirodivisas.Utils
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.util.ArrayList
+
+class CurrencyViewModel : ViewModel() {
+    private val _currencies = MutableStateFlow<List<String>>(emptyList())
+    private val currencies: StateFlow<List<String>> get() = _currencies
+
+    init {
+        loadCurrencies()
+    }
+
+    private fun loadCurrencies() {
+        viewModelScope.launch {
+            val currencyList = listOf(
+                "EUR", "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT",
+                "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF",
+                "CHF", "CLP", "CNY", "COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN",
+                "ETB", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD",
+                "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD",
+                "JPY", "KES", "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD",
+                "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN",
+                "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR",
+                "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP",
+                "SLE", "SLL", "SOS", "SRD", "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY",
+                "TTD", "TVD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES", "VND", "VUV", "WST", "XAF",
+                "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMW", "ZWL"
+            )
+            _currencies.value = currencyList
+        }
+    }
+
+    @Composable
+    fun CurrencyListScreen(viewModel: CurrencyViewModel = this@CurrencyViewModel): ArrayList<String> {
+        val currencies by viewModel.currencies.collectAsState()
+
+        return currencies.toList() as ArrayList<String>
+    }
+}
